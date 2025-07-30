f0478795ca54f410eae549c4a4c4f8706f7c2136
Martín García <tin.geka@gmail.com>
2025-07-29 21:45:11 -0300

    fix: remove test block

--- mu-plugins/enfantterrible-models/assets/blocks/post-types/fotoperiodismo/test/index.js
+++
@@ -1,9 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-
-import { BlockEdit } from './edit';
-import metadata from './block.json';
-
-registerBlockType(metadata, {
-	edit: BlockEdit,
-	save: () => null,
-});
