7f6bb48b07a1dbb6d2141eae9c46f525b94b89aa
Martín García <tin.geka@gmail.com>
2025-07-31 13:26:53 -0300

    feat: refactor block registration and template logic for Fotoperiodismo post type
    - Introduces new property `$blocks` to the Blocks class.
    - Better document property `$blocks_path`
    - Introduces `set_model_blocks` method for better management of the model blocks.
    - Introduces `get_model_blocks_json_files` method to retrieve all block.json files.
    - Renames `register_models_blocks` to `register_model_blocks` and now iterates through the new $blocks property.
    - Add `set_model_allowed_blocks` to restrict allowed blocks to only those registered by the model.
    - Add `set_model_blocks_template` to define a default block template for the model, excluding specific blocks.

--- mu-plugins/enfantterrible-models/src/PostTypes/Fotoperiodismo/Inc/Blocks.php
+++ mu-plugins/enfantterrible-models/src/PostTypes/Fotoperiodismo/Inc/Blocks.php
@@ -47,7 +47,21 @@ class Blocks implements ModuleInterface {
 	private Logger $logger;
 
 	/**
-	 * Constructor for the Meta class.
+	 * The model blocks path.
+	 *
+	 * @var string
+	 */
+	private string $blocks_path;
+
+	/**
+	 * The blocks object, containing its metadata via block.json declaration.
+	 *
+	 * @var array
+	 */
+	private array $blocks;
+
+	/**
+	 * Constructor for the Blocks class.
 	 *
 	 * Initializes the model name, model type, and logger instance.
 	 */
@@ -55,7 +69,8 @@ class Blocks implements ModuleInterface {
 		$this->model_name  = 'fotoperiodismo';
 		$this->model_type  = 'post';
 		$this->logger      = LoggerFactory::get_logger( 'enfantterrible-models', 'fotoperiodismo.blocks' );
-		$this->blocks_path = ENFANTTERRIBLE_MODELS_DIST_PATH . '/blocks/post-types/' . $this->model_name . '/';
+		$this->blocks_path = untrailingslashit( ENFANTTERRIBLE_MODELS_DIST_PATH ) . '/blocks/post-types/' . $this->model_name . '/';
+		$this->blocks      = $this->set_model_blocks();
 	}
 
 	/**
@@ -63,7 +78,9 @@ class Blocks implements ModuleInterface {
 	 *
 	 * @return bool
 	 */
-	public function can_register() {
+	public function can_register(): bool {
+			// This module is not intended for auto-registration via the framework.
+		// Its 'register' method is called directly by a Controller class.
 		return false;
 	}
 
@@ -72,12 +89,14 @@ class Blocks implements ModuleInterface {
 	 *
 	 * @return void
 	 */
-	public function register() {
+	public function register(): void {
 		$this->setup_asset_vars(
 			dist_path: ENFANTTERRIBLE_MODELS_DIST_PATH,
 			fallback_version: ENFANTTERRIBLE_MODELS_VERSION
 		);
-		add_action( 'init', [ $this, 'register_models_blocks' ] );
+		add_action( 'init', [ $this, 'register_model_blocks' ] );
+		add_action( 'init', [ $this, 'set_model_blocks_template' ] );
+		add_action( 'init', [ $this, 'set_model_allowed_blocks' ] );
 	}
 
 	/**
@@ -85,32 +104,31 @@ class Blocks implements ModuleInterface {
 	 *
 	 * @return void
 	 */
-	public function register_models_blocks() {
-		if ( ! file_exists( $this->blocks_path ) ) {
-			return;
-		}
+	public function register_model_blocks(): void {
+		$blocks = $this->blocks;
 
-		$block_json_files = glob( $this->blocks_path . '**/block.json', GLOB_BRACE );
-		$block_names      = [];
-
-		if ( empty( $block_json_files ) ) {
-			return;
-		}
-
-		foreach ( $block_json_files as $filename ) {
-			$block_folder = dirname( $filename );
+		foreach ( $blocks as $block ) {
+			$block_path   = ENFANTTERRIBLE_MODELS_PATH . $block['relative_path'];
+			$block_folder = dirname( $block_path );
 			$block        = register_block_type_from_metadata( $block_folder );
 
 			if ( ! $block ) {
-				continue;
+				$this->logger->error( 'Error registering block.', [ 'block_folder' => $block_folder ] );
 			}
-
-			$block_names[] = $block->name;
 		}
+	}
 
+	/**
+	 * Modifies the allowed block types for the Fotoperiodismo post type,
+	 * allowing only the blocks registered by this module.
+	 *
+	 * @return void
+	 */
+	public function set_model_allowed_blocks(): void {
+		$blocks_names = array_keys( $this->blocks );
 		add_filter(
 			'allowed_block_types_all',
-			function ( array|bool $allowed_blocks, \WP_Block_Editor_Context $context ) use ( $block_names ): array|bool {
+			function ( array|bool $allowed_blocks, \WP_Block_Editor_Context $context ) use ( $blocks_names ): array|bool {
 				$allowed_post_types = [ $this->model_name ];
 
 				if ( ! isset( $context->post ) || ! in_array( $context->post->post_type, $allowed_post_types, true ) ) {
@@ -119,13 +137,13 @@ class Blocks implements ModuleInterface {
 
 				if ( true === $allowed_blocks ) {
 					return array_merge(
-						[], // Add core blocks here if desired
-						$block_names
+						[], // Placeholder for adding a list of core blocks if desired.
+						$blocks_names
 					);
 				}
 
 				if ( is_array( $allowed_blocks ) ) {
-					return array_merge( $allowed_blocks, $block_names );
+					return array_merge( $allowed_blocks, $blocks_names );
 				}
 
 				return $allowed_blocks;
@@ -133,5 +151,97 @@ class Blocks implements ModuleInterface {
 			10,
 			2
 		);
+	}
+
+	/**
+	 * Sets the default block template for the Fotoperiodismo post type.
+	 *
+	 * The template is built by iterating over the model's blocks, while excluding
+	 * specific blocks from the template.
+	 *
+	 * @return void
+	 */
+	public function set_model_blocks_template(): void {
+		$post_type_object = get_post_type_object( $this->model_name );
+		$blocks_names     = array_keys( $this->blocks );
+
+		// Blocks to exclude, stored in an array
+		$excluded_blocks = [
+			'enfantterrible-models/authors-item',
+		];
+
+		// Create the template array
+		$template_blocks = array();
+		foreach ( $blocks_names as $block_name ) {
+			// Check if the current block name is in the excluded array
+			if ( ! in_array( $block_name, $excluded_blocks ) ) {
+				// Add the block to the template if it's not excluded
+				$template_blocks[] = array( $block_name, array() );
+			}
+		}
+
+		$post_type_object->template      = $template_blocks;
+		$post_type_object->template_lock = 'all'; // Optional
+	}
+
+
+	/**
+	 * Builds an array of model blocks metadata.
+	 *
+	 * @return array An associative array where keys are block names (string)
+	 * and values are arrays containing the following keys:
+	 * - 'relative_path' (string): The relative path to the block folder.
+	 * - 'metadata' (object): The JSON-decoded block metadata.
+	 */
+	private function set_model_blocks(): array {
+		$blocks_json_files = $this->get_model_blocks_json_files();
+
+		if ( empty( $blocks_json_files ) ) {
+			return [];
+		}
+
+		$blocks = [];
+
+		foreach ( $blocks_json_files as $block_json ) {
+			$block_content = file_get_contents( $block_json );
+
+			if ( ! $block_content ) {
+				$this->logger->error( 'Could not read block.json or file is empty.', [ 'file' => $block_json ] );
+				continue;
+			}
+
+			$block_metadata  = json_decode( $block_content );
+			$block_json_path = str_replace( ENFANTTERRIBLE_MODELS_PATH, '', $block_json );
+
+			// Add JSON validation
+			if ( json_last_error() !== JSON_ERROR_NONE || ! isset( $block_metadata->name ) ) {
+				$this->logger->error( 'Invalid block.json', [ 'file' => $block_json ] );
+				continue;
+			}
+
+			$blocks[ $block_metadata->name ] = [
+				'relative_path' => $block_json_path,
+				'metadata'      => $block_metadata,
+			];
+		}
+
+		return $blocks;
+	}
+
+	/**
+	 * Get an array of file paths to all block.json files in the blocks path.
+	 *
+	 * @return array File paths to block.json files, or an empty array on error.
+	 */
+	private function get_model_blocks_json_files(): array {
+
+		$block_json_files = glob( $this->blocks_path . '**/block.json', GLOB_BRACE );
+
+		if ( ! $block_json_files ) {
+			$this->logger->error( 'No blocks found or path is invalid.', [ 'block_json_files' => $block_json_files ] );
+			return [];
+		}
+
+		return $block_json_files;
 	}
 }
